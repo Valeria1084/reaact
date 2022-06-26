1aa2efef1abbd9379171b84011c0596444a8d13a
Valeria1084 <leraponomarenko1084@gmail.com>
2022-06-26 16:31:14 +0300

    leson

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Users} from "./components";
+
+function App() {
+  return (
+    <div >
+      <Users/>
+
+    </div>
+  );
+}
+
+export default App;
